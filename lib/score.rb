d260b2e734abfcf16ebc0243dcbf45b90c46cd0c
Thiago Paixão <tpaxadev@gmail.com>
2016-04-21 03:48:16 -0300

    well done

--- lib/score.rb
+++ lib/score.rb
@@ -1,24 +1,54 @@
 module Bowling
   class Score
-    attr_reader :pins
+    attr_reader :pins, :first_in_row
 
     def initialize(pins = [0] * 20)
       @pins = pins
+      @first_in_row = 0
     end
 
     def evaluate
-      first_in_row, row, score = [0, 0, 0]
-
-      # require 'pry'; binding.pry
+      row, score = [0, 0]
 
       while row < 10
-        score += (pins[first_in_row].to_i + pins[first_in_row + 1].to_i)
 
-        first_in_row += 2
+        if spare?
+          score += spare_result
+          @first_in_row += 2
+        elsif strike?
+          score += strike_result
+          @first_in_row += 1
+        else
+          score += row_result
+          @first_in_row += 2
+        end
+
         row += 1
       end
 
       score
     end
+
+    private
+
+      def spare?
+        row_result == 10
+      end
+
+      def strike?
+        pins[first_in_row].to_i == 10
+      end
+
+      def row_result
+        pins[first_in_row].to_i + pins[first_in_row + 1].to_i
+      end
+
+      def spare_result
+        row_result + pins[first_in_row + 2].to_i
+      end
+
+      def strike_result
+        pins[first_in_row].to_i + pins[first_in_row + 1].to_i + pins[first_in_row + 2].to_i
+      end
   end
 end
--- spec/lib/score_spec.rb
+++ spec/lib/score_spec.rb
@@ -21,4 +21,18 @@ describe Bowling::Score do
     expect(score.evaluate).to eq(20)
   end
 
+  it 'should calculate spare when a row equals 10' do
+    score = Bowling::Score.new([8, 2, 5] + [0] * 17)
+    expect(score.evaluate).to eq(20)
+  end
+
+  it 'should calculate strike when a single play equals 10' do
+    score = Bowling::Score.new([10, 3, 4] + [0] * 16)
+    expect(score.evaluate).to eq(24)
+  end
+
+  it 'should calculate a perfect game' do
+    score = Bowling::Score.new([10] * 12)
+    expect(score.evaluate).to eq(300)
+  end
 end
